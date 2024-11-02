e61e707ae4fc0356c32883203caa7fdeb4185776
Siddharth Sahay <sahaysid@gmail.com>
2024-11-02 14:20:45 +0100

    script to start db and make extension

--- start_db.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo systemctl status postgresql
-sudo -u postgres psql dna
---
+++ start_db_make_extension.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo systemctl status postgresql
+
+make
+sudo make install
+
+sudo -u postgres psql -d dna -c "DROP EXTENSION IF EXISTS dna CASCADE;"
+sudo -u postgres psql -d dna -c "CREATE EXTENSION dna;"
+
+sudo -u postgres psql dna
