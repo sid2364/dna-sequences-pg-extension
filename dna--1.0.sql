869fd8f35db2998937816c2f354abb556564cd58
Siddharth Sahay <sahaysid@gmail.com>
2024-11-21 12:14:27 +0100

    WIP: kmer type and functions, equality doesn't work

--- dna--1.0.sql
+++ dna--1.0.sql
@@ -116,42 +116,127 @@ CREATE OPERATOR <-> (
 * K-mers
 ******************************************************************************/
 
-/* First arg is cast from string to DNA with "dna_cast_from_text" directly */
+/* Input/Output functions */
+CREATE OR REPLACE FUNCTION kmer_in(cstring)
+  RETURNS kmer
+  AS 'MODULE_PATHNAME'
+  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE OR REPLACE FUNCTION kmer_out(kmer)
+  RETURNS cstring
+  AS 'MODULE_PATHNAME'
+  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE OR REPLACE FUNCTION kmer_recv(internal)
+  RETURNS kmer
+  AS 'MODULE_PATHNAME'
+  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE OR REPLACE FUNCTION kmer_send(kmer)
+  RETURNS bytea
+  AS 'MODULE_PATHNAME'
+  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+/* Type definition */
+CREATE TYPE kmer (
+  internallength = 16,  -- Fixed size: 4 bytes for length + 8 bytes for bit_sequence (total 12 bytes, aligned to 16 bytes)
+  input          = kmer_in,
+  output         = kmer_out,
+  receive        = kmer_recv,
+  send           = kmer_send,
+  alignment      = int
+);
+
+/* Casting Functions */
+CREATE OR REPLACE FUNCTION kmer(text)
+  RETURNS kmer
+  AS 'MODULE_PATHNAME', 'kmer_cast_from_text'
+  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE OR REPLACE FUNCTION text(kmer)
+  RETURNS text
+  AS 'MODULE_PATHNAME', 'kmer_cast_to_text'
+  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE CAST (text AS kmer) WITH FUNCTION kmer(text) AS IMPLICIT;
+CREATE CAST (kmer AS text) WITH FUNCTION text(kmer);
+
+/* Constructor */
+CREATE FUNCTION kmer_construct(text)
+  RETURNS kmer
+  AS 'MODULE_PATHNAME', 'kmer_constructor'
+  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+/* Operators */
+CREATE FUNCTION equals(kmer, kmer)
+  RETURNS boolean
+  AS 'MODULE_PATHNAME', 'kmer_equals'
+  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE FUNCTION kmer_ne(kmer, kmer)
+  RETURNS boolean
+  AS 'MODULE_PATHNAME', 'kmer_ne'
+  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE OPERATOR ~= (
+  LEFTARG = kmer, RIGHTARG = kmer,
+  PROCEDURE = equals,
+  COMMUTATOR = ~=, NEGATOR = <>
+);
+
+CREATE OPERATOR = (
+    LEFTARG = kmer, RIGHTARG = kmer,
+    PROCEDURE = equals,
+    COMMUTATOR = ~=, NEGATOR = <>
+);
+
+CREATE OPERATOR <> (
+  LEFTARG = kmer, RIGHTARG = kmer,
+  PROCEDURE = kmer_ne,
+  COMMUTATOR = <>, NEGATOR = ~=
+);
+
+/* Length */
+CREATE FUNCTION length(kmer)
+  RETURNS int
+  AS 'MODULE_PATHNAME', 'kmer_length'
+  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+/*
+Generate K-mers from DNA
+
+First arg is cast from string to DNA with "dna_cast_from_text" directly
+Returns a set of k-mers (of type kmer!)
+*/
 CREATE FUNCTION generate_kmers(dna dna, k int)
-RETURNS SETOF text
+RETURNS SETOF kmer
 AS 'MODULE_PATHNAME', 'generate_kmers'
 LANGUAGE C IMMUTABLE STRICT;
 
-CREATE FUNCTION kmer_equals(text, text) RETURNS boolean
-AS 'MODULE_PATHNAME', 'kmer_equals'
-LANGUAGE C IMMUTABLE STRICT;
-
-CREATE FUNCTION starts_with(text, text) RETURNS boolean
+CREATE FUNCTION starts_with(kmer, kmer) RETURNS boolean
 AS 'MODULE_PATHNAME', 'starts_with'
 LANGUAGE C IMMUTABLE STRICT;
 
-CREATE OPERATOR = (
-    LEFTARG = text,
-    RIGHTARG = text,
-    PROCEDURE = kmer_equals
-); /* This is different from dna equals! */
-
 CREATE OPERATOR ^@ (
-    LEFTARG = text,
-    RIGHTARG = text,
+    LEFTARG = kmer,
+    RIGHTARG = kmer,
     PROCEDURE = starts_with
 );
+--
+--CREATE OPERATOR CLASS kmer_ops DEFAULT FOR TYPE kmer USING btree AS
+--    OPERATOR 1 =,
+--    FUNCTION 1 btcompare(kmer, kmer);
 
 /******************************************************************************
 * For Qkmer pattern search
 ******************************************************************************/
 
-CREATE FUNCTION contains(text, text) RETURNS boolean
+CREATE FUNCTION contains(text, kmer) RETURNS boolean
 AS 'MODULE_PATHNAME', 'contains'
 LANGUAGE C IMMUTABLE STRICT;
 
 CREATE OPERATOR @> (
     LEFTARG = text,
-    RIGHTARG = text,
+    RIGHTARG = kmer,
     PROCEDURE = contains
 );
